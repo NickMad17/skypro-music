d623eb5eed04c23887943b2f3403223ff1279d9a
Maady <vlad030911@gmail.com>
2023-09-30 14:54:44 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Footer from "./components/Footer/Footer";
 import NavMenu from "./components/NavMenu/NavMenu";
 import SideBar from "./components/SideBar/SideBar";
 import TrackList from "./components/TrackList/TrackList";
-import * as S from "./App.styles"
+import * as S from "./App.styles";
 
 function App() {
   return (
